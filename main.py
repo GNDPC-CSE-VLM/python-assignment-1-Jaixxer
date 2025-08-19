9b9e01af19ec78b22679b7cde664a26ce000713a
Jaiveer Singh <jaiveer2112@gmail.com>
2025-08-20 02:21:53 +0530

    Completed Assignment1_SectionB

--- main.py
+++ main.py
@@ -2,10 +2,57 @@
 # Write your answers below each question
 
 # Q11: Swap variables
-# Your code here
+
+#Using a temporary variable
+a =10
+b=20
+print("Value of a before swapping: ",a)
+print("Value of b before swapping: ",b)
+temp = a
+a= b
+b= temp
+print("Value of a after swapping: ",a)
+print("Value of b after swapping: ",b)
+
+#Without using a temporary variable
+a =10
+b=20
+print("Value of a before swapping: ",a)
+print("Value of b before swapping: ",b)
+a += b
+b =a -b
+a-=b
+print("Value of a after swapping: ",a)
+print("Value of b after swapping: ",b)
+
 
 # Q12: Number pattern
-# Your code here
+value = "6 "
+for i in range(0,6):
+  print(value*6)
+
 
 # Q13: Factorial OR Password System
-# Your code here
+number = int(input("Enter the number: "))
+factorial = 1
+for i in range (1, number+1):
+  factorial *= i
+print(f"The factorial of {number} is {factorial}")
+
+
+#Password System
+attempt = 0
+password = "admin123"
+logged_in = False
+
+while attempt < 3:
+  login = input("Please enter the password: ")
+  if login == password:
+    print("Access Granted")
+    logged_in = True
+    break
+  else:
+    attempt+=1
+
+if logged_in == False:
+  print("Access Denied")
